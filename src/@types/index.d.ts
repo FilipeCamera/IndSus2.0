927ee122e18273fcaff072dfdceec580217e19d0
Filipe Camera <filipecameracastro@gmail.com>
2021-09-14 20:06:39 -0300

    creating @types

---
+++ src/@types/index.d.ts
@@ -0,0 +1,211 @@
+declare type Data = [
+  {
+    solo: {
+      fnpk: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      as: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      mo: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      pm: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      cpe: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+    };
+    bioPaisagem: {
+      rlapp: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      eap: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      bv: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      da: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      pai: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      id: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+    };
+    microClima: {
+      idac: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+    };
+    recursosNaturais: {
+      epm: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      ui: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      iva: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      bspm: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+    };
+    trajetoriaFam: {
+      ploap: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      nice: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+    };
+    segurançaAlimentar: {
+      apdas: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      das: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+    };
+    qualidadeVida: {
+      gtx: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      pcso: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      aa: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      abp: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+    };
+    organizacaoTrabalho: {
+      mof: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      ddi: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      drosp: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      orpa: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      pfap: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+    };
+    planejamentoProdutivo: {
+      rppsap: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      irinr: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      ecpm: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+    };
+    eficienciaEco: {
+      phec: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      cgr: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+    };
+    estabilidadeEco: {
+      diversidadeProdutiva: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      beneficiamentoProdutos: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+      dependenciaInsumos: {
+        case1: number;
+        case2: number;
+        case3: number;
+      };
+    };
+  },
+];
