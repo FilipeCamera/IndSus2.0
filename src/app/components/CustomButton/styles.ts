ecb214debff22e12b2a80b0109e540de641e2739
Filipe <filipecameracastro@gmail.com>
2022-02-27 23:24:39 -0300

    add linha maior

--- src/app/components/CustomButton/styles.ts
+++ src/app/components/CustomButton/styles.ts
@@ -1,3 +1,4 @@
+import {Colors} from '@styles';
 import styled from 'styled-components/native';
 
 export const CustomButtonStyle = styled.View`
@@ -9,5 +10,16 @@ export const ButtonStyle = styled.TouchableOpacity`
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
+  width: 100%;
   padding: 5px 8px;
+`;
+
+export const Alert = styled.View`
+  width: 12px;
+  height: 12px;
+  border-radius: 12px;
+  background: ${Colors.red};
+  position: absolute;
+  top: 5px;
+  right: 5px;
 `;
